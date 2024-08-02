6ddd9922f6e317f401c77e62258580f55b4091f1
armandotruglio <armando.truglio1@gmail.com>
2024-08-02 16:09:09 +0200

    Completed first Mileston

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('JS OK');
+
+/*
+MILESTONE 1
+Creo le variabili const per "Fizz" e "Buzz" 
+Creo un ciclo che vada da 1 a 100
+Per ogni iterazione controllo che il numero corrente sia multiplo di 3 e di 5
+Se il numero non è multiplo lo stampo, se è multiplo di 3 stampo Fizz, se è multiplo di 5 stampo Buzz
+*/
+
+const multipleOf3 = 'Fizz';
+const multipleOf5 = 'Buzz';
+
+for(i=1; i<=100; i++){
+    let n = '';
+
+    if(i % 3 === 0){
+        n += multipleOf3;
+    }
+    if(i % 5 === 0){
+        n += multipleOf5;
+    }
+    if(!n){
+        n = i;
+    }
+
+    console.log(n);
+}

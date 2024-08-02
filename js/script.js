c859c7abedf0ee3bab48d6441c997f8f7701045a
armandotruglio <armando.truglio1@gmail.com>
2024-08-02 16:25:23 +0200

    Finished Milestone 2

--- js/script.js
+++ js/script.js
@@ -8,8 +8,20 @@ Per ogni iterazione controllo che il numero corrente sia multiplo di 3 e di 5
 Se il numero non è multiplo lo stampo, se è multiplo di 3 stampo Fizz, se è multiplo di 5 stampo Buzz
 */
 
+
+/*
+MILESTONE 2:
+Recupero l'elemento dal DOM
+Creo una lista
+Per ogni iterazione del Ciclo aggiungo un elemento alla lista con l'output generato dall'iterazione
+*/
+
+const resultElement = document.getElementById('fizz-buzz');
+
 const multipleOf3 = 'Fizz';
 const multipleOf5 = 'Buzz';
+
+let list = '<ul>';
 
 for(i=1; i<=100; i++){
     let n = '';
@@ -24,5 +36,9 @@ for(i=1; i<=100; i++){
         n = i;
     }
 
-    console.log(n);
+    list += '<li>' + n + '</li>';
 }
+
+list += '</ul>'
+
+resultElement.innerHTML = content;
